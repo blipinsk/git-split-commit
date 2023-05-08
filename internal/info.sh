a8cfa1ec7a88de74cee6eb22b80a93a986e3ac92
Bartek Lipinski <contact@barteklipinski.com>
2023-05-08 12:04:38 +0200

    Version v1.0.2

--- internal/info.sh
+++ internal/info.sh
@@ -4,7 +4,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-VERSION="1.0.1"
+VERSION="1.0.2"
 export GIT_ALIAS="split-commit"
 
 function print_welcome {
